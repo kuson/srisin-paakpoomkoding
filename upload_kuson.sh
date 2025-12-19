5d2bd855a5f36c67001dfeaa1f2e9c4a4ef3e30c
Kuson Sintusingha <kusons@gmail.com>
2025-12-19 13:00:41 +0700

    feat: Add rsync script for media deployment and update APK download link text in `index.html`.

---
+++ upload_kuson.sh
@@ -0,0 +1,2 @@
+rsync -avz -e "ssh -i ~/.ssh/github-actions-caprover" ./videos/ ubuntu@161.118.244.149:~/Persistent_Directories/paakpoom-srisin/videos
+rsync -avz -e "ssh -i ~/.ssh/github-actions-caprover" ./assets/ ubuntu@161.118.244.149:~/Persistent_Directories/paakpoom-srisin/assets
