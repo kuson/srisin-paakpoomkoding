b9f390a6ef99d94d47326f9630c8d363788a557c
Kuson Sintusingha <kusons@gmail.com>
2025-12-19 13:27:19 +0700

    refactor: rename `upload_kuson.sh` to `upload_kuson_rsync_video_and_assets.sh` and add a comment.

--- upload_kuson_rsync_video_and_assets.sh
+++ upload_kuson_rsync_video_and_assets.sh
@@ -1,2 +1,3 @@
+#THIS IS SIMPLE BUT MANUAL AND REFERENCES KEYS:
 rsync -avz -e "ssh -i ~/.ssh/github-actions-caprover" ./videos/ ubuntu@161.118.244.149:~/Persistent_Directories/paakpoom-srisin/videos
 rsync -avz -e "ssh -i ~/.ssh/github-actions-caprover" ./assets/ ubuntu@161.118.244.149:~/Persistent_Directories/paakpoom-srisin/assets
